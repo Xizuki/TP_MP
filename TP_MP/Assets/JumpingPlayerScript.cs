982fc9caeacf5ea0a554ad7d7066ae8f4ad03de3
Xizuki <64124274+Xizuki@users.noreply.github.com>
2023-07-04 14:57:33 +0800

    jumping player

--- TP_MP/Assets/JumpingPlayerScript.cs
+++ TP_MP/Assets/JumpingPlayerScript.cs
@@ -18,6 +18,7 @@ public class JumpingPlayerScript : MonoBehaviour
     public float rbJumpStrength;
 
     public bool isGrounded;
+    public bool isFalling;
     public Transform feetPos;
 
     public void Awake()
@@ -36,13 +37,45 @@ public class JumpingPlayerScript : MonoBehaviour
     {
         
     }
-
+    public float inputValue;
+    public float scalar;
     // Update is called once per frame
     void Update()
     {
         Inputs();
 
         jumpingPlayerChildrenModel.transform.localEulerAngles = new Vector3(0,-playerUI.jumpingVectorIndicator.transform.eulerAngles.z,0);
+
+
+        if(rb.velocity.y < 4) { isFalling = true; }
+        else { isFalling = false; }
+
+        if (isFalling)
+            rb.AddForce(new Vector3(0, -8, 0));
+
+
+
+        
+
+        float transformedValue = TransformValue(inputValue, scalar);
+
+        Debug.Log("Transformed Value: " + transformedValue);
+
+    }
+
+    float TransformValue(float value, float scalar)
+    {
+        // Convert value to a non-linear scale
+        float transformedValue = 0;
+
+        for (int i = 0; i < inputValue;i++)
+        {
+            print(" i " + i + " = " + scalar * ((scalar * 50) / (50 + i)));
+            transformedValue += scalar * (((scalar * 50) / (50 + i)));
+        }
+
+        return transformedValue;
+
     }
 
     private void Inputs()
@@ -82,9 +115,9 @@ public class JumpingPlayerScript : MonoBehaviour
     }
     private void Jump()
     {
-        if (!isGrounded || jumpCharge<=0) { return; }
+        if (!isGrounded || jumpCharge <=0) { return; }
+        //float forceCalcs = TransformValue(rbJumpStrength * jumpCharge, scalar);
         rb.AddForce(playerUI.jumpingVectorIndicator.transform.up * rbJumpStrength * jumpCharge, ForceMode.Impulse);
-        isGrounded = false;
         jumpCharge = 0;
     }
 
