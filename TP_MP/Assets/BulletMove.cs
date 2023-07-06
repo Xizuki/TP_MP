203630075ef07923f495c5ecdcd3870ec0305a9c
Frostpenguin <82798946+Frostpenguin@users.noreply.github.com>
2023-07-06 14:10:39 +0800

    peashooter blast

--- TP_MP/Assets/BulletMove.cs
+++ TP_MP/Assets/BulletMove.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
-    public GameObject cactus;
     public int speed = 5;
+    public ParticleSystem explosion;
+    public ParticleSystem aura;
+    public MeshRenderer bulletMesh;
+    public SphereCollider bulletCollider;
 
     private void Update()
     {
@@ -15,5 +18,19 @@ public class BulletMove : MonoBehaviour
     public void FlyBullet()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (bulletCollider.enabled == false)
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+        explosion.Play();
+        bulletMesh.enabled = false;
+        bulletCollider.enabled = false;
+        aura.Stop();
     }
 }
