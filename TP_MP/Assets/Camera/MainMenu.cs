258b89d5c2021fc20191e390ee10b6ac34aa1091
dchief2 <nepugear@yahoo.com.sg>
2023-07-13 16:11:07 +0800

    13 Jul Progress
    - Main Menu enhancements, SFX, buttons, and gameplay start cutscene fixes complete

---
+++ TP_MP/Assets/Camera/MainMenu.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject UI;
+    public GameObject options;
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public AudioClip clipOptions;
+    public GameObject loadingTxtObject;
+    public TMP_Text loadingTxt;
+    public bool camUp;
+
+    string[] lore = {"Shiba is sneaking into the castle to save the princess.","The Shiba is able to jump high with it's powered boots."
+            ,"The Shiba is a dog of justice, with the goal of saving the princess!","The Shiba is able to defend itself with it's Mighty Shield."
+            ,"The Shiba has squishy cheeks that everyone loves.","Use your focus to charge up your jump to jump higher!","The Stopwatch is able to slow down time for you to get past enemies more easily."
+            ,"The Mighty Chicken is strong enough to carry the Shiba. It's the Shiba's loyal sidekick!","To protect the kingdom of Shibaland, the Shiba must reach the top of the castle!"
+            ,"The Shield is able to defend the Shiba from incoming attacks."};
+
+    void Start()
+    {
+        cam.SetActive(false);
+        camUp = false;
+        loadingTxtObject.SetActive(false);
+    }
+    
+    public void CamTwo()
+    {
+        cam.SetActive(true);
+        UI.SetActive(false);
+        StartCoroutine(WaitTime());
+    }
+
+    public void CamOff()
+    {
+        cam.SetActive(false);
+
+    }   
+
+    public void OnClickStart()
+    {
+        camUp = true;
+        audioSource.PlayOneShot(clip);
+        loadingTxtObject.SetActive(true);
+        loadingTxt.text = lore[Random.Range(0,lore.Length)];
+    }
+
+    public void OnClickOptions()
+    {
+        audioSource.PlayOneShot(clipOptions);
+        UI.SetActive(false);
+        options.SetActive(true);
+    }
+
+    IEnumerator WaitTime()
+    {
+        print("Scene Loading");
+        yield return new WaitForSeconds(5.75f);
+        SceneManager.LoadScene("JumpingGameScene_Dom");
+
+    }
+
+    private void Update()
+    {
+        if (camUp == true)
+        {
+            CamTwo();
+        }
+        else if (camUp == false)
+        {
+            CamOff();
+        }
+    }
+}
