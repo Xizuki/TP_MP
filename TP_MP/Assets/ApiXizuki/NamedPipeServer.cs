ef0c47265d3c9485359358728d3978a8d97606c3
Xizuki <xizuki12345@yahoo.com.sg>
2023-07-18 09:03:17 +0800

    a

--- TP_MP/Assets/ApiXizuki/NamedPipeServer.cs
+++ TP_MP/Assets/ApiXizuki/NamedPipeServer.cs
@@ -1,6 +1,128 @@
 using System;
 using System.IO;
 using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NamedPipeServer : MonoBehaviour
+{
+    private NamedPipeServerStream serverStream;
+    private CancellationTokenSource cancellationTokenSource;
+    private StreamReader reader;
+    private Task waitingTask;
+
+    public JumpingPlayerScript jumpingPlayer; // Reference to the JumpingPlayerScript
+    string lastestLine;
+
+    private void Awake()
+    {
+        jumpingPlayer = GetComponent<JumpingPlayerScript>(); // Get a reference to the JumpingPlayerScript component
+    }
+
+    private void Start()
+    {
+        // Create a named pipe server with a specific name
+        serverStream = new NamedPipeServerStream("MyNamedPipe", PipeDirection.In);
+        reader = new StreamReader(serverStream);
+
+        // Start waiting for a client to connect asynchronously
+        cancellationTokenSource = new CancellationTokenSource();
+        Debug.Log("Start() 1 ");
+        waitingTask = WaitForConnectionAsync(cancellationTokenSource.Token);
+        Debug.Log("Start() 2 ");
+    }
+
+    private async Task WaitForConnectionAsync(CancellationToken cancellationToken)
+    {
+        Debug.Log("WaitForConnectionAsync() 1 ");
+
+        try
+        {
+            // Wait for a client to connect asynchronously
+            await serverStream.WaitForConnectionAsync(cancellationToken);
+            Debug.Log("WaitForConnectionAsync() 2 ");
+
+            // Check if the pipe stream is broken
+            try
+            {
+                // Attempt to read or write to the pipe
+                serverStream.ReadByte();
+                Debug.Log("The pipe stream is not broken.");
+            }
+            catch (IOException ex) when (IsPipeBrokenException(ex))
+            {
+                Debug.Log("The pipe stream is broken.");
+            }
+
+            // Start reading messages from the connected client
+            ReadMessage();
+        }
+        catch (OperationCanceledException)
+        {
+            // The waiting task has been canceled, ignore the exception
+            return;
+        }
+        catch (Exception ex)
+        {
+            // Handle any other exceptions
+            Debug.LogError($"An error occurred while waiting for connection: {ex.Message}");
+        }
+    }
+
+    static bool IsPipeBrokenException(IOException ex)
+    {
+        const int ERROR_PIPE_NOT_CONNECTED = 233;
+        const int ERROR_NO_DATA = 232;
+
+        var errorCode = ex.HResult & 0xFFFF;
+        return errorCode == ERROR_PIPE_NOT_CONNECTED || errorCode == ERROR_NO_DATA;
+    }
+
+    private void ReadMessage()
+    {
+        Debug.Log("ReadMessage() 1 ");
+
+        while (serverStream.IsConnected)
+        {
+            Debug.Log("ReadMessage() 2 ");
+
+            if (lastestLine == "Null")
+            {
+                serverStream.Close();
+            }
+
+            if (lastestLine == "JumpCharge")
+            {
+                jumpingPlayer.namedPipeJumpCharging = true;
+            }
+
+            // Read the next line from the stream
+            lastestLine = reader.ReadLine();
+        }
+    }
+
+    private void Update()
+    {
+        jumpingPlayer.namedPipeJumpCharging = false;
+    }
+
+    private void OnDisable()
+    {
+        cancellationTokenSource.Cancel();
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        cancellationTokenSource.Cancel();
+      
+    }
+}
+
+/*using System;
+using System.IO;
+using System.IO.Pipes;
 using UnityEngine;
 
 public class NamedPipeServer : MonoBehaviour
@@ -20,41 +142,46 @@ public class NamedPipeServer : MonoBehaviour
 
     private void CheckPipeStream()
     {
-        using (NamedPipeServerStream pipeStream = serverStream)
-        {
-            // Wait for a client to connect
-            pipeStream.WaitForConnection();
 
-            // Check if the pipe stream is broken
-            try
-            {
-                // Attempt to read or write to the pipe
-                pipeStream.ReadByte();
-                Debug.Log("The pipe stream is not broken.");
-            }
-            catch (IOException ex) when (IsPipeBrokenException(ex))
-            {
-                Debug.Log("The pipe stream is broken.");
-            }
+        // Wait for a client to connect
+        serverStream.WaitForConnection();
+
+        // Check if the pipe stream is broken
+        try
+        {
+            // Attempt to read or write to the pipe
+            serverStream.ReadByte();
+            Debug.Log("The pipe stream is not broken.");
         }
+        catch (IOException ex) when (IsPipeBrokenException(ex))
+        {
+            Debug.Log("The pipe stream is broken.");
+        }
+
     }
 
 
     private void Start()
     {
-        // Create a new thread to avoid blocking the main Unity thread
-        System.Threading.Thread thread = new System.Threading.Thread(CheckPipeStream);
-        thread.Start();
-
         // Create a named pipe server with a specific name
         serverStream = new NamedPipeServerStream("MyNamedPipe", PipeDirection.In);
         print("new serverStream");
-        reader = new StreamReader(serverStream);
+        //reader = new StreamReader(serverStream);
         //print("BeginWaitForConnection 1");
 
+
+        print("BeginWaitForConnection 1");
+        
         serverStream.BeginWaitForConnection(OnClientConnected, null);
+        print("BeginWaitForConnection 2");
+
+        print("BeginWaitForConnection 3 if connected = " + serverStream.IsConnected);
 
         //print("BeginWaitForConnection 2");
+
+        // Create a new thread to avoid blocking the main Unity thread
+        //System.Threading.Thread thread = new System.Threading.Thread(CheckPipeStream);
+        //thread.Start();
     }
 
     static bool IsPipeBrokenException(IOException ex)
@@ -69,38 +196,43 @@ public class NamedPipeServer : MonoBehaviour
     {
         jumpingPlayer.namedPipeJumpCharging = false;
         //print("server.isconnected = " + serverStream.IsConnected);
-        System.Threading.Thread thread = new System.Threading.Thread(CheckPipeStream);
+        //System.Threading.Thread thread = new System.Threading.Thread(CheckPipeStream);
+        //print("Update if connected = " + serverStream.IsConnected);
+        //print("Update serverstream null = " + (serverStream == null));
 
     }
 
     private void OnClientConnected(System.IAsyncResult result)
     {
+        print("OnClientConnected()");
+
         ReadMessage();
     }
     public void ReadMessage()
     {
+        print("ReadMessage()");
         while (serverStream.IsConnected)
         {
             //if (!serverStream.IsConnected) { continue; }
             if (lastestLine == "Null") { serverStream.Close(); }
-            lastestLine =  reader.ReadLine();
-
-            print(lastestLine);
+            //lastestLine =  reader.ReadLine();
+            
+            //print(lastestLine);
             /*
             if(lastestLine == "Jump")
             {
                 jumpingPlayer.Jump();
             }
-            3*/
-            if (lastestLine == "JumpCharge")
+            3
+if (lastestLine == "JumpCharge")
             {
                 jumpingPlayer.namedPipeJumpCharging = true ;
             }
 
         }
 
-        reader.Close();
-        reader.Dispose();
+        //reader.Close();
+        //reader.Dispose();
         //if (serverStream.IsConnected)
         serverStream.Disconnect();
         serverStream.Close();
@@ -114,23 +246,31 @@ public class NamedPipeServer : MonoBehaviour
     }
     private void OnDisable()
     {
-        reader.Close();
-        reader.Dispose();
-        //if (serverStream.IsConnected)
-        serverStream.Disconnect();
-        serverStream.Close();
-        serverStream.Dispose();
+        //reader.Close();
+        //reader.Dispose();
+        if (serverStream.IsConnected)
+        {
+            serverStream.Disconnect();
 
+        }
 
+            serverStream.Close();
+            serverStream.Dispose();
+        
     }
     
     private void OnApplicationQuit()
     {
-        reader.Close();
-        reader.Dispose();
-       // if (serverStream.IsConnected)
-        serverStream.Disconnect();
-        serverStream.Close();
-        serverStream.Dispose();
+        //reader.Close();
+        //reader.Dispose();
+        if (serverStream.IsConnected)
+        {
+            serverStream.Disconnect();
+        }
+        
+            serverStream.Close();
+            serverStream.Dispose();
+        
     }
 }
+*/
