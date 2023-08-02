46bd72065cd071646792e8473e8b3afa8d17e4e9
Xizuki <64124274+Xizuki@users.noreply.github.com>
2023-08-02 16:26:59 +0800

    asd

--- Assets/Xi_TextureAutoTiling.cs
+++ Assets/Xi_TextureAutoTiling.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Xi_TextureAutoTiling : MonoBehaviour
 {
+    public Material mat;
+    public float scale;
+    private void OnValidate()
+    {
+        mat.mainTextureScale = new Vector2(transform.localScale.x , transform.localScale.y) * scale;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,6 @@ public class Xi_TextureAutoTiling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
