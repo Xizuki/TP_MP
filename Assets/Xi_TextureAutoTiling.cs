46cc26c57a812c2a92eed02b31ad9d2b0e991e52
Xizuki <64124274+Xizuki@users.noreply.github.com>
2023-08-04 10:15:28 +0800

    asd

--- Assets/Xi_TextureAutoTiling.cs
+++ Assets/Xi_TextureAutoTiling.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class Xi_TextureAutoTiling : MonoBehaviour
 {
-    public Material mat;
+    public Material sourceMat;
+    public Material matInstance;
     public float scale;
     private void OnValidate()
     {
-        mat.mainTextureScale = new Vector2(transform.localScale.x , transform.localScale.y) * scale;
+        //Texture2D newTextureInstance = new Texture2D(mat.mainTexture.width, mat.mainTexture.height);
+
+        //newTextureInstance.SetPixels(((Texture2D)mat.mainTexture).GetPixels());
+
+        //textureInstance = newTextureInstance;
+
+        //mat.mainTexture = textureInstance;
+
+        if (matInstance == sourceMat || matInstance == null)
+        {
+
+            Renderer renderer = GetComponent<Renderer>();
+            sourceMat = renderer.material;
+
+            matInstance = Instantiate(sourceMat);
+            renderer.material = matInstance;
+
+            matInstance.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y) * scale;
+        }
+        else
+        {
+            matInstance.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y) * scale;
+        }
     }
     // Start is called before the first frame update
     void Start()
