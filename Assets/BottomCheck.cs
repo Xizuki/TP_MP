cebab7ad3b4f1a40f38c1458a926c823f298a32b
Frostpenguin <82798946+Frostpenguin@users.noreply.github.com>
2023-08-07 11:26:27 +0800

    n

---
+++ Assets/BottomCheck.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomCheck : MonoBehaviour
+{
+    public float baseSpeed;
+    public float speed;
+    public float distance;
+    private bool movingRight = true;
+    private float timer = 1.5f;
+    private bool hit = false;
+
+    public Transform groundCheck;
+
+    private Animator animator;
+
+    RaycastHit groundInfo;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out groundInfo, distance);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(180 , -270, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(180 , -90, 0);
+                movingRight = true;
+            }
+        }
+
+        if (hit == true)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            speed = 5f;
+            timer = 1.3f;
+            hit = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hit = true;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Attack");
+            animator.SetTrigger("Attack");
+            speed = 0f;
+        }
+    }
+}
