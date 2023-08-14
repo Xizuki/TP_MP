33a383dcf81aaad2b384d4f711c2b4b6f2867cc8
Frostpenguin <82798946+Frostpenguin@users.noreply.github.com>
2023-08-14 16:43:54 +0800

    n

--- Assets/Script/WorldGeneration.cs
+++ Assets/Script/WorldGeneration.cs
@@ -6,10 +6,19 @@ public class WorldGeneration : MonoBehaviour
 {
     public GameObject shibaKnight;
     public Vector3 heroPosition;
-    public List<GameObject> spawnRoom;
     public GameObject spawnTop;
     public int roomType;
+    public List<GameObject> spawnRoom;
     public float spawnBottom = 80f;
+    public int spawnedRooms = 0;
+    private int randomRangeIndex = 0;
+
+    private int[][] randomRanges = new int[][]
+    {
+        new int[] { 0, 3 },
+        new int[] { 4, 6 },
+        new int[] { 7, 9 }
+    };
 
 
     // Start is called before the first frame update
@@ -33,5 +42,17 @@ public class WorldGeneration : MonoBehaviour
         roomType = Random.Range(0, 3);
         spawnTop = GameObject.Instantiate(spawnRoom[roomType], spawnTop.transform.position,
         spawnTop.transform.rotation).GetComponentInChildren<SpawnTopScript>().gameObject;
+        spawnedRooms = spawnedRooms + 1;
+
+        /*
+        if (spawnedRooms > 0 && spawnedRooms % 3 == 0)
+        {
+            randomRangeIndex = (randomRangeIndex + 1) % randomRanges.Length;
+        }
+
+        int[] currentRange = randomRanges[randomRangeIndex];
+        int randomValue = currentRange[Random.Range(0, currentRange.Length)];
+
+        Debug.Log("Random Value: " + randomValue);*/
     }
 }
