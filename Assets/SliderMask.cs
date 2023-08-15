2d9b0e64f3aae1e7d280865c78ab18850d415861
Xizuki <xizuki12345@yahoo.com.sg>
2023-08-15 11:47:30 +0800

    asd

--- Assets/SliderMask.cs
+++ Assets/SliderMask.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderMask : MonoBehaviour
 {
+    public Slider slider;
+    public Slider maskedSlider;
+
     // Start is called before the first frame update
     void Start()
     {
+        //if (maskedSlider == null)
+        //    DupeSlider();
+    }
+
+    public void OnValidate()
+    {
+    //    if (maskedSlider == null)
+    //        DupeSlider();
+
+
+    //    maskedSlider.value = slider.value;
+
+    }
+
+    public void DupeSlider()
+    {
+        //slider = GetComponent<Slider>();
+        //maskedSlider = GameObject.Instantiate(gameObject, gameObject.transform).GetComponent<Slider>();
+        //maskedSlider.fillRect.GetComponent<Image>().material = null;
+        //maskedSlider.name = slider.name+"_Color";
+
+        //UnityEditor.EditorApplication.delayCall += () =>
+        //{
+        //    DestroyImmediate(maskedSlider.GetComponent<SliderMask>());
+        //};
+
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        maskedSlider.value = slider.value;
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("MyComponent was disabled or removed.");
+
+       // Destroy(maskedSlider);
     }
 }
