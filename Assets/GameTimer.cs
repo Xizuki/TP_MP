4dbd61501cd76249d1ec3b478de138595e9dff56
Frostpenguin <82798946+Frostpenguin@users.noreply.github.com>
2023-08-23 14:43:35 +0800

    n

---
+++ Assets/GameTimer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timetTest;
+    public GameObject endMenu;
+    public float timer;
+    public bool gameEnded;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timetTest.text = "Timer:" + Mathf.Round(timer);
+        if(timer <= 0)
+        {
+            Time.timeScale = 0;
+            endMenu.gameObject.SetActive(true);
+            gameEnded = true;
+        }
+    }
+}
