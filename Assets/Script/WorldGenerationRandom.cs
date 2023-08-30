d4b639bd6bf6e702fe6d25c4575fdf8674d0b2fa
Xizuki <xizuki12345@yahoo.com.sg>
2023-08-30 11:40:59 +0800

    asd

---
+++ Assets/Script/WorldGenerationRandom.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerationRandom : MonoBehaviour
+{
+    public GameObject shibaKnight;
+    public Vector3 heroPosition;
+    public GameObject spawnTop;
+    public int roomType;
+    public List<GameObject> spawnRoom;
+    public float spawnBottom = 80f;
+    public int spawnedRooms = 0;
+
+    void Start()
+    {
+        heroPosition = shibaKnight.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        heroPosition = shibaKnight.transform.position;
+        if (heroPosition.y > (spawnTop.transform.position.y) - spawnBottom)
+        {
+            RoomSpawn();
+        }
+    }
+
+    void RoomSpawn()
+    {
+        spawnTop = GameObject.Instantiate(spawnRoom[roomType], spawnTop.transform.position,
+        spawnTop.transform.rotation).GetComponentInChildren<SpawnTopScript>().gameObject;
+        roomType += 1;
+
+        if (roomType >= 3)
+        {        
+            Debug.Log(roomType);
+
+            roomType = 0;
+            Debug.Log("hit");
+        }
+        spawnedRooms = spawnedRooms + 1;
+    }
+}
